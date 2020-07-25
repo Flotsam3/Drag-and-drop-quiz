6d993867102ffd2b3d9565f8d525e39273d71646
Joker <admin@e.mail.de>
2020-07-26 00:52:39 +0200

    003 - Created intro text by JS

--- js/start.js
+++ js/start.js
@@ -11,7 +11,6 @@ const svgRecStart = document.querySelector('.cube__side--front rect');
 const svgRecRight = document.querySelector('.cube__side--right rect');
 const svgRecTop = document.querySelector('.cube__side--top rect');
 const layer = document.querySelector('.layer');
-// const quizInfo = document.querySelector('.quiz-info');
 const scene = document.querySelector('.scene');
 const pane = document.querySelector('.pane');
 const aTag = document.querySelector('a');
@@ -32,8 +31,9 @@ function animateStart(){
 }
     
 buttonName.addEventListener('click', animateStartName);
-    
+
 function animateStartName(){
+    playerName = document.querySelector('#name').value;
     buttonName.classList.add('button-disabled');
     svgRecTop.classList.add('dynamic-border');
     cube.classList.add('top');
@@ -41,24 +41,38 @@ function animateStartName(){
 
 buttonLevel.addEventListener('click', animateStartLevel);
 
+
 function animateStartLevel(){
+    playerLevel = document.querySelector('input[name = level]:checked').value
+    let levelTime = 0;
+
+    switch (playerLevel) {
+        case 'easy':
+            levelTime = 45;
+            break;
+        case 'fair':
+            levelTime = 30;
+            break;
+        case 'challenging':
+            levelTime = 20;
+    }
+    
     buttonLevel.classList.add('button-disabled');
     cube.removeChild(cubeFront);
     cube.removeChild(cubeLeft);
     cube.removeChild(cubeRight);
     cube.removeChild(cubeBack);
-    svgRecTop.classList.remove('dynamic-border');
-
-    // setTimeout(() => {
-    //     document.querySelector('.start').classList.add('slide-startpage');
-    // }, 1500);
-    
+    svgRecTop.classList.remove('dynamic-border');    
     scene.classList.add('--fade-out');
     
     setTimeout(() => {
+        console.log(playerName);
+        console.log(playerLevel);
+        console.log(levelTime);
+        const introText = document.querySelector('.pane p');
+        introText.innerHTML = `Nice to have you here ${playerName}, </br></br> during this quiz you will be presented with five terms for each round and your task is to bring all of them into the right order by using drag and drop. </br></br> Each correctly assigned term will earn you five points.</br></br> Your prefered level of difficulty is "${playerLevel}", therefore you have ${levelTime} seconds to accomplish each of the 20 rounds. </br></br></br><span>Have fun!</span>`
         buttonInfo.style.removeProperty('visibility');
         pane.classList.add('--fade-in');
         aTag.classList.add('--fade-in');
-        // buttonInfo.addEventListener('click', fadeOut);
     }, 2000);
 }
