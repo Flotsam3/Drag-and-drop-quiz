c5fa545a1b18a8e6bcfb79ad713c3bbb0e486e0b
Joker <admin@e.mail.de>
2020-07-28 01:29:44 +0200

    005 - Score for player and computer is running, solution running, multiple rounds running

--- js/main.js
+++ js/main.js
@@ -6,10 +6,13 @@ const buttonCard = document.querySelector('#button-card')
 const card = document.querySelector('.card__content');
 const myTimer = document.querySelector('.timer span');
 const infoText = document.querySelector('.info p');
-const barText = document.querySelectorAll('.bar');
+let barText = document.querySelectorAll('.bar');
 let rankIndex = [0, 1, 2, 3, 4];
+let rankIndexComputer = [0, 1, 2, 3, 4];
+let selectionComputer = [];
 let quizRound = 0;
 let randomNumber = 0;
+let randomNumberComputer = 0;
 
 infoText.textContent = `Get ready to start ${playerName}!`;
 myTimer.textContent = levelTime;
@@ -22,8 +25,6 @@ function getExerciseData(){
         return response.json();
     })
     .then((exerciseData) => {
-        console.log(exerciseData);
-        console.log(exerciseData[0].options[1]);
         
         buttonCard.addEventListener('click', rotateCard);
 
@@ -33,29 +34,35 @@ function getExerciseData(){
         }
         
         function fillBars(){
+            rankIndex = [0, 1, 2, 3, 4];
+            rankIndexComputer = [0, 1, 2, 3, 4];
+            selectionComputer = [];
+
             for (let i = 0; i < 5; i++) {
                 randomNumber = Math.floor(Math.random() * rankIndex.length);
-                barText[i].innerHTML += exerciseData[quizRound].options[rankIndex[randomNumber]];
+                randomNumberComputer = Math.floor(Math.random() * rankIndexComputer.length);
+                barText[i].innerHTML += exerciseData[quizRound].options[rankIndex[randomNumber]]; // fill bars with terms
+                selectionComputer.push(rankIndexComputer[randomNumberComputer]); // fill array with random numbers for the computer answers
                 rankIndex.splice(randomNumber, 1);
+                rankIndexComputer.splice(randomNumberComputer, 1);
             }
         }
 
         function rotateCard(){
         
             if (card.classList.contains('card--rotate')){
-                showExerciseInfo();
                 card.classList.remove('card--rotate');
+                resetCard();
+                quizRound++;
             } else {
                 showExerciseInfo();
                 card.classList.add('card--rotate');
+                countdown();
             }
-            
-            countdown();
         }
 
         function countdown(){
             let myTime = levelTime;
-            console.log('reached countdown');
             
             const myCountdown = setInterval(() => {
                 if (myTime === 5){
@@ -67,23 +74,69 @@ function getExerciseData(){
                 myTime--;
                 if (myTime < 0){
                     clearInterval(myCountdown);
-                    console.log('timer below zero');
                     showSolution();
                 }
             }, 1000);
         }
         
         function showSolution(){
-            console.log('reached solution');
-            const solution = document.querySelectorAll('.solution');
+            let barText = document.querySelectorAll('.bar');
+            let solution = document.querySelectorAll('.solution');
+            const number = document.querySelectorAll('.number');
+            const playerScore = document.querySelector('.player-value');
+            let playerScoreCounter = parseInt(playerScore.textContent);
+            const computerScore = document.querySelector('.comp-value');
+            let computerScoreCounter = parseInt(computerScore.textContent);
+            const searchString = exerciseData[quizRound];
+            let fullHouse = 0;
+            
             solution.forEach((element, index) => {
+                const answerIndex = searchString.options.indexOf(barText[index].childNodes[2].nodeValue); // Index of the answer inside the JSON data
+                element.childNodes[0].nodeValue = searchString.solutions[answerIndex];
+                number[index].textContent = searchString.rank[answerIndex];
+                
+                if (barText[index].childNodes[2].nodeValue === exerciseData[quizRound].options[index]) { // Check if the player got the right answer
+                    fullHouse++;
+                    playerScoreCounter += 5;
+                    element.style.backgroundColor = '#3282b8';
+                } else {
+                    element.style.backgroundColor = '#e44803';
+                };
+                
+                if (selectionComputer[index] === index) {
+                    computerScoreCounter += 5;
+                }
                 
                 setTimeout(() => {
                     element.style.visibility = "visible";
                     element.classList.add('solution--slide');
                 }, 1500 * index);
+                
+                setTimeout(() => {
+                    playerScore.textContent = playerScoreCounter;
+                    computerScore.textContent = computerScoreCounter;
+                    if (fullHouse === 5) {
+                        document.querySelector('.card__back').classList.add('full-house');
+                    }
+                }, 7500);
             });
         
+        }
+
+        function resetCard() {
+            let solution = document.querySelectorAll('.solution');
+
+            solution.forEach(element => {
+                element.classList.add('solution--fade-out')
+                element.style.visibility = "hidden";
+                element.classList.remove('solution--slide');
+            });
+
+            document.querySelector('.card__back').classList.remove('full-house');
+
+            for (let i = 0; i < 5; i++) {
+                barText[i].childNodes[2].nodeValue = "";
+            }
         }
     })
 }
